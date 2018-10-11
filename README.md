21d39365cf2ceba739ab2c2a74cc34cdf719bbb7
Jan Mewes <jmewes@experimental-software.com>
2018-10-11 08:22:45 +0200

    Use different tool detaching process from terminal

--- README.md
+++ README.md
@@ -21,3 +21,4 @@ TBD
 ## References
 - https://stackoverflow.com/questions/645992/bash-sleep-until-a-specific-time-date
 - https://superuser.com/a/483464
+- https://unix.stackexchange.com/questions/3886/difference-between-nohup-disown-and
--- reminder.sh
+++ reminder.sh
@@ -5,4 +5,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 REMINDER_TIMER=$1
 REMINDER_TEXT=$2
 
-nohup ${SCRIPT_DIR}/schedule_notification.sh ${REMINDER_TIMER} ${REMINDER_TEXT} &
+${SCRIPT_DIR}/schedule_notification.sh ${REMINDER_TIMER} ${REMINDER_TEXT} &
+disown
