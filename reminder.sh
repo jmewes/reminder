3a5cd05c595af1d6b5a2ca3502bce036bdc67004
Jan Mewes <jmewes@experimental-software.com>
2020-05-01 13:24:46 +0200

    Add support for URLs in notifications on MacOS

--- reminder.sh
+++ reminder.sh
@@ -2,12 +2,56 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-REMINDER_TIME=$1
-REMINDER_TEXT=$2
+usage () {
+     cat << EOF
+DESCRIPTION:
+The purpose of this tool is to display popup notifications at specific times.
+ 
+SYNOPSIS:
+$0 add <time> <message> [<url>]
+$0 list
+$0 stop <pid>
+$0 -h
 
-if [[ ! "${REMINDER_TIME}" = *":"*  ]]; then
-  REMINDER_TIME_IN_SECONDS=$(($(date +%s) + ((${REMINDER_TIME} * 60))))
-  REMINDER_TIME=$(date -d @${REMINDER_TIME_IN_SECONDS} '+%H:%M')
+ACTIONS:
+  add       Add a new reminder
+  list      Show scheduled reminders
+  stop      Stop the reminder with the given process id.  
+
+OPTIONS:
+  <time>    The time at which the popup notice is to be open in 24h format (HH:MM), e.g. 15:25.
+  <message> The message to be displayed in the popup notice.
+  <url>     The URL to be linked in the popup notice. (Currently only supported on MacOS.)
+  <pid>     The operating system process id of the scheduled reminder.
+  -h        Show this message.
+EOF
+}
+
+ACTION=$1
+
+if [[ -z "${ACTION}" || "${ACTION}" = "-h" ]]; then
+    usage
+    exit 0
 fi
 
-nohup ${SCRIPT_DIR}/schedule_notification.sh ${REMINDER_TIME} "${REMINDER_TEXT}" > /dev/null 2>&1 &
+if [[ "${ACTION}" == "add" ]]; then
+    REMINDER_TIME=$2
+    REMINDER_TEXT=$3
+    REMINDER_URL=$4
+
+    if [[ ! "${REMINDER_TIME}" = *":"*  ]]; then
+      REMINDER_TIME_IN_SECONDS=$(($(date +%s) + ((${REMINDER_TIME} * 60))))
+      REMINDER_TIME=$(date -d @${REMINDER_TIME_IN_SECONDS} '+%H:%M')
+    fi
+    
+    if [[ -n "${REMINDER_URL}" ]]; then
+        if [[ ! "${REMINDER_URL}" = http* ]]; then
+            echo "ERROR: Reminder URL '${REMINDER_URL}' has an invalid format." >&2
+            exit 1
+        fi
+    fi
+
+    nohup ${SCRIPT_DIR}/schedule_notification.sh -t ${REMINDER_TIME} -m "${REMINDER_TEXT}" -u "${REMINDER_URL}" > /dev/null 2>&1 &
+fi
+
+
--- schedule_notification.sh
+++ schedule_notification.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
 
-REMINDER_TIME=$1
-REMINDER_TEXT=$2
+usage () {
+     cat << EOF
+DESCRIPTION:
+This script will sleep until the specified time and then display a popup
+notification with the tools available for the respective operating system.
+ 
+SYNOPSIS:
+$0 -m message -t time [-u url]
+ 
+OPTIONS:
+    -m   The message to be displayed in the popup notice.
+    -t   The time at which the popup notice is to be open in 24h format (HH:MM), e.g. 15:25.
+    -u   The URL to be linked in the popup notice. (Currently only supported on MacOS.)
+    -h   Show this message.
+    -?   Show this message.
+EOF
+}
+
+while getopts "m: t: u: h ?" option ; do
+     case $option in
+          m)   MESSAGE="${OPTARG}"
+               ;;
+          t)   TIME="${OPTARG}"
+               ;;
+          u)   URL="${OPTARG}"
+               ;;
+          h )  usage
+               exit 0;;
+          ? )  usage
+               exit 0;;
+     esac
+done
+ 
+if [[ -z ${MESSAGE} ]]; then
+    echo "ERROR: parameter '-m' not set"
+    usage
+    exit 1
+fi
+
+if [[ -z ${TIME} ]]; then
+    echo "ERROR: parameter '-t' not set"
+    usage
+    exit 1
+fi
 
 function sleep_until {
     seconds=$(( $1 - $(date +%s) ))
@@ -9,12 +51,16 @@ function sleep_until {
     sleep $seconds
 }
 
-REMINDER_TIMESTAMP=$(date -d "${REMINDER_TIME}" +%s)
+REMINDER_TIMESTAMP=$(date -d "${TIME}" +%s)
 
 sleep_until $REMINDER_TIMESTAMP
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    terminal-notifier -title "Reminder" -message "$REMINDER_TEXT"
+    if [[ -n ${URL} ]]; then
+        terminal-notifier -title "Reminder" -message "$MESSAGE" -open "$URL"
+    else
+        terminal-notifier -title "Reminder" -message "$MESSAGE"
+    fi
 else
-    notify-send  -u critical -t 0 "Reminder" $REMINDER_TEXT
+    notify-send  -u critical -t 0 "Reminder" "$MESSAGE"
 fi
